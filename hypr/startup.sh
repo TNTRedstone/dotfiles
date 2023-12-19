7afccbe947924510b3514fc4a22b150ffab6ce3f
= <=>
2023-12-19 16:37:43 -0500

    add programs to hyprland

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -1,4 +1,6 @@
+python ~/Documents/coding-projects/startup_hue_lights/main.py &
 ~/.config/hypr/wallpaper.sh &
 waybar &
 picom &
 /usr/bin/syncthing -no-browser -no-restart -logflags=0 &
+~/.nix-profile/bin/aw-qt &
